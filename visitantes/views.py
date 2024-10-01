b0cfff1af6927c024e28dbe1540d32fab4929d68
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-01 17:44:26 -0300

    feat: template registrar visitante

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
-# Create your views here.
+from visitantes.forms import VisitanteForm
+
+def registrar_visitante(request):
+    form = VisitanteForm()
+    
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+        
+        if form.is_valid():
+            visitante = form.save(commit=False)
+            
+            visitante.registrado_por = request.user.porteiro
+            visitante = form.save()
+            
+            messages.success(request,'Visitante registrado com sucesso')
+            return redirect('index')
+        
+    context ={
+        'nome_pagina': 'Registrar visitante',
+        'form': form
+    }
+    
+    return render(request,'registrar_visitante.html',context)
