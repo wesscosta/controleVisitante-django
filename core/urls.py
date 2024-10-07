2b8aa0135b396cd1e76cf28f518d6200eb7f30a3
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-07 17:51:33 -0300

    feat: logout

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,10 @@ from visitantes.views import registrar_visitante, informacoes_visitante, finaliz
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index ,name='index'),
-    path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name= 'login'),
+    
+    path('login/',auth_views.LoginView.as_view(template_name = 'login.html'), name= 'login'),
+    path('logout/',auth_views.LoginView.as_view(template_name = 'logout.html'), name= 'logout' ),
+    
     path('registrar-visitante/', registrar_visitante, name='registrar_visitante'),
     path('visitante/<int:pk>', informacoes_visitante, name='informacoes_visitante'),
     path('visitante/<int:pk>/finalizar-visita', finalizar_visita, name='finalizar_visita'),
