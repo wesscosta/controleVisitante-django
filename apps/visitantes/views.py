7dcea1687c3c5b95a2e5228b3555966a0e75c488
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-07 17:36:56 -0300

    feat: atualizar e finalizar visita

--- apps/visitantes/views.py
+++ apps/visitantes/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseNotAllowed
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from visitantes.forms import AutorizaVisitanteForm, VisitanteForm
+from .forms import AutorizaVisitanteForm, VisitanteForm
 from .models import Visitante
 
 @login_required
@@ -31,10 +33,36 @@ def registrar_visitante(request):
 def informacoes_visitante(request, pk):
     
     visitante = get_object_or_404(Visitante,pk=pk)
+    form = AutorizaVisitanteForm(request.POST, instance= visitante)
     
+    if form.is_valid():
+        visitante = form.save(commit=False)
+        visitante.status = "EM_VISITA"
+        visitante.horario_autorizacao = timezone.now()  
+        
+        visitante.save()
+        
+        messages.success(request, 'Entrada do visistante autorizada com sucesso')
+        return redirect('index')
+        
     context ={
         'nome_pagina': 'Informações Visitante',
-        'visitante': visitante
+        'visitante': visitante,
+        'form': form,
     }
     
     return render(request,'informacoes_visitante.html',context)
+
+@login_required
+def finalizar_visita(request, pk):
+    
+    if request.method == 'POST':
+        visitante = get_object_or_404(Visitante,pk=pk)
+        visitante.status = 'FINALIZADO'
+        visitante.horario_saida = timezone.now()
+        visitante.save()
+        
+        messages.success(request, 'Entrada do visistante autorizada com sucesso')
+        return redirect('index')
+    else:
+        return HttpResponseNotAllowed(['POST'],'Metodo não permitido')        
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from dashboard.views import index
-from visitantes.views import registrar_visitante, informacoes_visitante
-from django.contrib.auth import views as auth_views
+from visitantes.views import registrar_visitante, informacoes_visitante, finalizar_visita
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name= 'login'),
     path('registrar-visitante/', registrar_visitante, name='registrar_visitante'),
     path('visitante/<int:pk>', informacoes_visitante, name='informacoes_visitante'),
+    path('visitante/<int:pk>/finalizar-visita', finalizar_visita, name='finalizar_visita'),
 ]
